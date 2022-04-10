14fe09c6ba102590b93fae18db4bab789227b2aa
Vanson Lin <vanson@google.com>
2022-04-10 17:00:02 -0400

    upload more stuff

--- bot.py
+++ bot.py
@@ -19,11 +19,11 @@ WHITELIST_CHANNEL_NAME = 'whitelistmembers'
 
 
 client = discord.Client(intents=intents)
-whitelist_channel = None
+whitelist_channel = None  # Instance of the whitelist channel
 
-def print_all_member(guild):
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+# def print_all_member(guild):
+#     members = '\n - '.join([member.name for member in guild.members])
+#     print(f'Guild Members:\n - {members}')
 
 
 @client.event
@@ -39,14 +39,9 @@ async def on_ready():
 async def on_member_update(before, after):
     print('on_member_udate')
     print(f'name: {before.name}')
-    print(f'old roles: {before.roles}, new roles: {after.roles}')
+    print(f'old: {before}, new: {after}')
     if is_adding_whitelist_role_event(before, after):
-        await whitelist_channel.send(f'welcome to the channel, {before.name}, now leave your address')
-        # dm_channel = before.dm_channel
-        # if dm_channel == None:
-        #     print('channel == None, creating a new dm channel')
-        #     dm_channel = await before.create_dm()
-        # await dm_channel.send('hello, send me your address')
+        await whitelist_channel.send(f'welcome to the channel, {before.name}! Now leave your address')
 
 
 def is_adding_whitelist_role_event(before, after):
@@ -55,32 +50,37 @@ def is_adding_whitelist_role_event(before, after):
     print(roles)
     return len(roles) == 1 and roles.pop().name == WHITELIST_ROLE_NAME
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
 
 @client.event
 async def on_message(message):
-    print('on_message')    
+    print('on_message')
     if message.author == client.user:
         return
     if message.content.lower() == 'ping!':
         await message.channel.send('pong!')
-    if isinstance(message.channel, discord.DMChannel):
-        try_collect_address(message)
+    if message.channel is whitelist_channel:
+        print('msg from whitelist_channel')
+        await try_collect_address(message)
 
-def try_collect_address(message):
-    if has_whitelist_role(message.author.roles) and is_giving_valid_address(message.content):
-        write_to_sheet(message.author, message.content)
+
+async def try_collect_address(message):
+    if not has_whitelist_role(message.author.roles):
+        return
+    if not is_giving_valid_address(message.content):
+        return
+
+    result = write_to_sheet(message.author, message.content)
+    if result:
+        await whitelist_channel.send(f' Thanks {message.author.name}! Your address is recorded.')
+        await message.delete()
+
 
 def is_giving_valid_address(msg):
     return len(msg.split()) == 1
 
 def write_to_sheet(user, address):
     print(f'id: {user.id}, name: {user.name}, address: {address}')
+    return True
     # TODO: implement this
 
 def has_whitelist_role(roles):
